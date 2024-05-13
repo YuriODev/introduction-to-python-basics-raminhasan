54765aa18d4d3cd96f92c21a3ca73966cd0b5381
raminhasan <ramin.hasan@outlook.com>
2024-05-13 08:36:12 +0000

    solution 6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,10 @@
 # Exercise 6
-# Your solution comes here
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+if a % b == 0 or b % a == 0:
+  print("YES")
+
+else:
+  print("NO")
