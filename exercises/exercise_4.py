7973db746e77a9f9eb5635a88bcbe6c8e577b65e
raminhasan <ramin.hasan@outlook.com>
2024-05-13 08:23:19 +0000

    solution

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,16 @@
 # Exercise 4
-# Your solution comes here
+
+user_input = input("Enter a four digit number: ")
+
+if len(user_input) < 4:
+  N = 4 - len(user_input)
+  
+  user_input = user_input.zfill(N + len(user_input))
+
+reversed = user_input[::-1]
+
+if reversed == user_input:
+  print(1)
+
+else:
+  print(0)
