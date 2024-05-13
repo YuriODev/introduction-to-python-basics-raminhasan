f3acdf14976266279ac1729be01c82a14302b3dc
raminhasan <ramin.hasan@outlook.com>
2024-05-13 08:08:03 +0000

    solutions

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,18 @@
 # Exercise 3
-# Your solution comes here
+
+n = int(input('enter a number of seconds: '))
+
+hours = n // 3600
+minutes = (n % 3600) // 60
+seconds = (n % 3600) % 60
+
+if seconds < 10:
+  seconds = f"0{seconds}"
+  
+if minutes < 10:
+  minutes = f"0{minutes}"
+  
+if hours < 10:
+  hours = f"0{hours}"
+
+print(f"{n} equates to {hours}:{minutes}:{seconds}")
