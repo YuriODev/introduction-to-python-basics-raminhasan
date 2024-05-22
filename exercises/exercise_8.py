2d673cb703e598e96ca3043364c9ab29ee0e08a8
raminhasan <ramin.hasan@outlook.com>
2024-05-22 12:25:56 +0000

    sol 9

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,12 +1,13 @@
 # Exercise 8
 
-array = []
+item = input("Enter a number: ")
 
 for i in range(3):
-  item = int(input("Enter a number: "))
   array.append(item)
 
 array.sort()
 
 for x in range(3):
   print(array[x])
+
+print(array)
--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,17 @@
 # Exercise 9
-# Your solution comes here
+
+h = int(input('enter a value for hours between 0 and 12: '))
+m = int(input('enter a value for minutes between 0 and 60: '))
+s = int(input('enter a value for seconds between 0 and 60: '))
+
+h %= 12
+m %= 60
+s %= 60
+
+rotations_hours = h * 30
+rotations_minutes = m * 0.5
+rotations_seconds = s *  (1/120)
+
+total_rots = rotations_hours + rotations_minutes + rotations_seconds
+
+print(total_rots)
