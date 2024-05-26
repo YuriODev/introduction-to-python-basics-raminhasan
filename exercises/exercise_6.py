9f8420c482f646954d2d4d317177118f7774b5ad
raminhasan <ramin.hasan@outlook.com>
2024-05-26 10:34:23 +0000

    s6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -3,4 +3,4 @@
 a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 
-divisibility = 'yes' * (a % b == 0) or 'no' * (a % b != 0)
+divisibility = 'yes' * (a % b == 0) + 'no' * (a % b > 0)
