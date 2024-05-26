a0dca4aaf55a7dd413061ed3baafff0d7142c7af
raminhasan <ramin.hasan@outlook.com>
2024-05-26 10:47:52 +0000

    s8 deffo

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,11 +1,11 @@
 # Exercise 8
 
-item_one = input("Enter a number: ")
-item_two = input("Enter a number: ")
-item_three = input("Enter a number: ")
+a = int(input())
+b = int(input())
+c = int(input())
 
-min = item_one * (item_one <= item_two and item_one <= item_three) + item_two * (item_two <= item_one and item_two <= item_three) + item_three * (item_three <= item_one and item_three <= item_two)
-mid = item_one * ((item_one >= item_two and item_one <= item_three) + (item_one >= item_three and item_one <= item_two)) + item_two * ((item_two >= item_one and item_two <= item_three) + (item_two >= item_three and item_two <= item_three)) + item_three * ((item_three >= item_two and item_three <= item_one) + (item_three >= item_one and item_three <= item_two))
-max = item_one * (item_one >= item_two and item_one >= item_three) + item_two * (item_two >= item_one and item_two >= item_three) + item_three * (item_three >= item_one and item_three >= item_two)
+min = a * (a <= b and a <= c) + b * (b < a or b < c) + c * (c < a and c < b)
+max = a * (a >= b and a >= c) + b * (b > a or b > c) + c * (c > a and c > b)
+mid = a + b + c - min - max
 
 print(f'{min}, {mid}, {max}')
