859beba59f145dee5e82acdfa985565f0f2cfbf8
raminhasan <ramin.hasan@outlook.com>
2024-05-26 10:33:15 +0000

    s9

--- exercises/exercise_10.py
+++ exercises/exercise_10.py
@@ -1,3 +1,10 @@
 # Exercise 10
 
+a = int(input('how many degrees has the hour hand moved? '))
 
+hrs = a / 60
+mins = hrs * 30
+
+degrees = mins * 6
+
+print(degrees)
--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -4,14 +4,14 @@ h = int(input('enter a value for hours between 0 and 12: '))
 m = int(input('enter a value for minutes between 0 and 60: '))
 s = int(input('enter a value for seconds between 0 and 60: '))
 
-h %= 12
-m %= 60
-s %= 60
-
 rotations_hours = h * 30
-rotations_minutes = m * 0.5
+rotations_minutes = m * 6
 rotations_seconds = s *  (1/120)
+
+print(rotations_hours)
+print(rotations_minutes)
+print(rotations_seconds)
 
 total_rots = rotations_hours + rotations_minutes + rotations_seconds
 
-print(total_rots // 1)
+print(total_rots)
