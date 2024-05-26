9fa227252746775b6c8a98346aa1804d74acd9cd
raminhasan <ramin.hasan@outlook.com>
2024-05-26 10:55:38 +0000

    s9

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,12 +1,25 @@
 # Exercise 9
 
-h = int(input('enter a value for hours between 0 and 12: '))
-m = int(input('enter a value for minutes between 0 and 60: '))
-s = int(input('enter a value for seconds between 0 and 60: '))
+running = True
+
+while running:
+  h = int(input('enter a value for hours between 0 and 12: '))
+  m = int(input('enter a value for minutes between 0 and 60: '))
+  s = int(input('enter a value for seconds between 0 and 60: '))
+
+  if 0 <= h <= 12 and 0 <= m <= 60 and 0 <= s <= 60:
+    running = False
+    break
+
+  else:
+    pass
+
+minute_fraction_hour = m / 6
+second_fraction_hour = s / 3600
 
 rotations_hours = h * 30
-rotations_minutes = m * 6
-rotations_seconds = s *  (1/120)
+rotations_minutes = minute_fraction_hour * 6
+rotations_seconds = second_fraction_hour *  (1/120)
 
 print(rotations_hours)
 print(rotations_minutes)
