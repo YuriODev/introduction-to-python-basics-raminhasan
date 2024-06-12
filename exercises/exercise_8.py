8057ad630e0f794e64e64d8040d91863330a88d1
raminhasan <ramin.hasan@outlook.com>
2024-06-12 12:29:56 +0000

    sol89

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -8,4 +8,4 @@ min = a * (a <= b and a <= c) + b * (b < a or b < c) + c * (c < a and c < b)
 max = a * (a >= b and a >= c) + b * (b > a or b > c) + c * (c > a and c > b)
 mid = a + b + c - min - max
 
-print(f'{min}, {mid}, {max}')
+print(f'{min}\n{mid}\n{max}')
--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,30 +1,9 @@
 # Exercise 9
 
-running = True
+h = int(input())
+m = int(input())
+s = int(input())
 
-while running:
-  h = int(input('enter a value for hours between 0 and 12: '))
-  m = int(input('enter a value for minutes between 0 and 60: '))
-  s = int(input('enter a value for seconds between 0 and 60: '))
+angle = 0.5 * (60 * h + m + s / 60)
 
-  if 0 <= h <= 12 and 0 <= m <= 60 and 0 <= s <= 60:
-    running = False
-    break
-
-  else:
-    pass
-
-minute_fraction_hour = m / 6
-second_fraction_hour = s / 3600
-
-rotations_hours = h * 30
-rotations_minutes = minute_fraction_hour * 6
-rotations_seconds = second_fraction_hour *  (1/120)
-
-print(rotations_hours)
-print(rotations_minutes)
-print(rotations_seconds)
-
-total_rots = rotations_hours + rotations_minutes + rotations_seconds
-
-print(total_rots)
+print(angle)
